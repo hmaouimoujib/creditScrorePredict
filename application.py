25905b821a332201e7a502adc3ab10eccdc89f0a
hmaoui_moujib <moujibhmaoui17@gmail.com>
2023-09-13 19:24:18 +0200

    init

---
+++ application.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+application = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@application.route('/')
+def home():
+    return render_template('index.html')
+
+@application.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+    return render_template('index.html', prediction_text='la réponse a votre requète {}'.format(output))
+
+
+
+if __name__ == "__main__":
+    application.run(host="0.0.0.0",port=80, debug=True)
